0efc52b7b366eae11f9ef4d07583469104777cc4
EYOB <eyobzawude76@gmail.com>
2026-04-28 21:21:52 +0300

    Add  file

--- src/app/registration/page.jsx
+++ src/app/registration/page.jsx
@@ -17,7 +17,7 @@ export default function Home() {
     city: ""
   })
 
-  const API_URL = "http://unireg-1.onrender.com"
+  const API_URL = "https://unireg-1.onrender.com"
 
   const courseData = {
     Engineering: {
