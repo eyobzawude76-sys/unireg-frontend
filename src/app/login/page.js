8ef4fa74fcfd40150279396590b63042064865d8
EYOB <eyobzawude76@gmail.com>
2026-04-28 21:10:45 +0300

    Add  file

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -13,7 +13,7 @@ export default function LoginPage() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/admin/login", {
+      const res = await fetch("https://unireg-1.onrender.com/admin/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
